8262b695f34da6be93aad9f2d8fa594e8e2ec585
tramntn0611 <dstramntn@gmail.com>
2024-06-20 03:17:03 +0700

    Update sentiment_shopee.py

--- sentiment_shopee.py
+++ sentiment_shopee.py
@@ -479,17 +479,15 @@ elif  choice == 'Thông tin nhà hàng':
 
             leftcol1, rightcol1 = st.columns(2)
             with leftcol1:
-                with st.form(key='RatingForm'):
-                    st.markdown(
-                    f"""
-                    <div style="text-align: center;">
-                        <h1>{star_rating}</h1>
-                        <h1>{rating}</h1>
-                        <p><strong>{total_rat} đánh giá</strong></p>
-                    </div>
-                    """, 
-                    unsafe_allow_html=True
-                )
+                f"""
+                <div style="display: flex; flex-direction: column; align-items: center;">
+                    <h1 style="margin: 0;">{star_rating}</h1>
+                    <h1 style="margin: 0;">{rating}</h1>
+                    <p style="margin: 0;"><strong>{total_rat} đánh giá</strong></p>
+                </div>
+                """, 
+                unsafe_allow_html=True
+            )
             with rightcol1:
                 df_plot_rat = df_selection.groupby(['Restaurant']).sum()[['9-10', '7-8', '5-6', '3-4', '1-2']]
                 for restaurant, row in df_plot_rat.iterrows():
